948363f709cad19d46f65ad26c794a876b50f94e
ANDRES MANRIQUE ARDILA <a.manrique@sis.virtual.uniandes.edu.co>
2019-08-15 22:41:42 -0500

    Modal added with its video

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ $('#nav').affix({
 	var index=0;
 	$(document).scroll(function(){
 		var top = $('#skills').height()-$(window).scrollTop();
-		console.log(top)
 		if(top<-300){
 			if(index==0){	
 			
@@ -87,14 +86,38 @@ $('#nav').affix({
 			});
 		}
 	});
-	
-  	// Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
+
+
+
 
 }());
 
 
 }
+function modal(pModal,pBtn,pPos){
+   // Get the modal
+var modal = document.getElementById(pModal);
+
+// Get the button that opens the modal
+var btn = document.getElementById(pBtn);
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[pPos];
+
+// When the user clicks the button, open the modal 
+  modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
 main();
+modal();
